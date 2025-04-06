4c7627a623524b4ebb7161340a61c8c88a32dc96
Zhanyl <asanakunovajanyla@gmail.com>
2025-04-07 02:58:07 +0600

    Правки в Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "../Header/Header.scss"
 import { Link } from 'react-router-dom'
-
 import logo from "../../assets/svg/logo.svg"
 import search from "../../assets/svg/search.svg"
 import cart from "../../assets/svg/cart.svg"
