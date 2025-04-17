4134a1ad4c19f8459d5e2350a87d8637672f5236
Albinka <a00.ismailova@gmail.com>
2025-04-17 19:56:36 +0600

    created cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { myRouter } from "./router";
+// import Shopping from './ShoppingCart/Shopping';
+
 
 function App() {
+  // const Shopping = () => {
   return (
     <>
       <div className="wrapper">
         <RouterProvider router={myRouter} />
+        {/* <div className="App">
+          <Shopping />
+        </div> */}
+        
       </div>
     </>
   );
+
 }
 export default App;
