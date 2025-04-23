35bd2091b62c489c0a2183f3c51f4534062db533
zannatnurmahammatova@gmail.com <zannatnurmahammatova@gmail.com>
2025-04-24 03:52:31 +0600

    wish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "../Header/Header.scss"
 import { Link } from 'react-router-dom'
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux"; 
 
 import logo from "../../assets/svg/logo.svg"
 import search from "../../assets/svg/search.svg"
@@ -10,23 +11,25 @@ import cart from "../../assets/svg/cart.svg"
 import login from "../../assets/svg/login.svg"
 import wishlist from "../../assets/svg/wishlist.svg"
 
-
 function Header() {
-    const navigate = useNavigate();
-  
-    const handlePersonClick = () => {
-      if (auth.currentUser) {
-        navigate("/profile");
-      } else {
-        navigate("/register");
-      }
-    };
+  const navigate = useNavigate();
+  const wishlistItems = useSelector((state) => state.wishlist.items);
+
+
+  const handlePersonClick = () => {
+    if (auth.currentUser) {
+      navigate("/profile");
+    } else {
+      navigate("/register");
+    }
+  };
+
   return (
     <div>
       <div className='header container'>
 
         <Link to="/" className='logo'>
-          <img src={logo} alt="" />
+          <img src={logo} alt="logo" />
         </Link>
 
         <div className='lists'>
@@ -38,24 +41,29 @@ function Header() {
 
         <div className='search'>
           <Link to="/search">
-            <img src={search} alt="" />
+            <img src={search} alt="search" />
           </Link>
           <Link className='wishlist' to="/wishlist">
-            <img src={wishlist} alt="" />
+            <div className="icon-wrapper">
+              <img src={wishlist} alt="wishlist" />
+              {wishlistItems.length > 0 && (
+                <span className="wishlist-badge">{wishlistItems.length}</span>
+              )}
+            </div>
           </Link>
           <Link to="/cart">
-            <img src={cart} alt="" />
+            <img src={cart} alt="cart" />
           </Link>
           <Link to={"/login"}>
             <div className='login'>
               <button onClick={handlePersonClick}>
-                <img src={login} alt="" />
+                <img src={login} alt="login" />
                 Login
               </button>
             </div>
           </Link>
-
         </div>
+
       </div>
     </div>
   )
